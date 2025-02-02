c4aa516ca44c2c72fcdf683ec710800563b88d3c
Görkem Yılmaz <gorkem.yilmaz@studenti.unipd.it>
2025-02-02 18:17:42 +0300

    Create Dashboard Stats model

--- backend/apps/dashboard/admin.py
+++ backend/apps/dashboard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import DashboardStats
 
 # Register your models here.
+admin.site.register(DashboardStats) # Register DashboardStats model
--- backend/apps/dashboard/models.py
+++ backend/apps/dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class DashboardStats(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # User
+    quit_date = models.DateField() # Quit smoking date
+    money_saved = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) # Money saved
+    cigarettes_avoided = models.IntegerField(default=0) # Cigarettes avoided
+    co_level = models.DecimalField(max_digits=5, decimal_places=2, default=0.00) # CO level  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quit_date}"
