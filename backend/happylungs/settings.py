574147d86fe8af4af41224f0d5dcfa255d1d6a97
Görkem Yılmaz <gorkem.yilmaz@studenti.unipd.it>
2025-03-01 14:29:38 +0300

    Add logging in settings

--- backend/happylungs/settings.py
+++ backend/happylungs/settings.py
@@ -148,6 +148,26 @@ DATABASES = {
     )
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'django_errors.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
