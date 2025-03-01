60a353c8328fd4460e28daf1577aa322d86f96dc
Görkem Yılmaz <gorkem.yilmaz@studenti.unipd.it>
2025-03-01 15:52:13 +0300

    deployment is hard

--- backend/happylungs/settings.py
+++ backend/happylungs/settings.py
@@ -215,15 +215,27 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "backend/staticfiles",
-]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Add this for serving frontend assets
-WHITENOISE_ROOT = BASE_DIR / "backend/staticfiles"
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
